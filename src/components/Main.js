dbe07641a665115c839c9054048b8327abd03c89
Jennifer <jennifer.raab16@gmail.com>
2022-08-26 08:58:04 +0200

    styling Header and Articles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,11 @@
 import Article from "./Article";
+import "./Main.css";
 import { useState, useEffect } from "react";
 
-
 function Main() {
-
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState();
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -16,14 +15,13 @@ function Main() {
     //const url = `http://hn.algolia.com/arch?query=${query}`;    //error testing
 
     request(url);
-
   }, [query]);
 
   const request = (u) => {
     fetch(u)
       .then((response) => {
         if (!response.ok) {
-          throw Error('Failed to fetch from this resource');
+          throw Error("Failed to fetch from this resource");
         }
         return response.json();
       })
@@ -35,29 +33,28 @@ function Main() {
         setError(err.message);
         setArticles([]);
       });
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     setQuery(userInput);
-    setUserInput('');
+    setUserInput("");
   };
 
   const getContent = () => {
     // if (!articles.length) {
     //   return <div className="alertMessage">No result matches the query</div>;
     // }
-    return (
-      articles.map((article) => {
-        return (<Article
+    return articles.map((article) => {
+      return (
+        <Article
           article={article}
           key={article.objectID}
-        // key={crypto.randomUUID()}              //check if objectID is really unique
+          // key={crypto.randomUUID()}              //check if objectID is really unique
         />
-        )
-      })
-    );
+      );
+    });
   };
 
   return (
@@ -73,42 +70,49 @@ function Main() {
         <div className="footer-container">
           <div>
             <a href="https://news.ycombinator.com/newsguidelines.html">
-              Guidelines |
+              Guidelines
             </a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://news.ycombinator.com/newsfaq.html">FAQ |</a>
+            <a href="https://news.ycombinator.com/newsfaq.html">FAQ</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://news.ycombinator.com/lists">Lists |</a>
+            <a href="https://news.ycombinator.com/lists">Lists</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://github.com/HackerNews/API">API |</a>
+            <a href="https://github.com/HackerNews/API">API</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://news.ycombinator.com/security.html">Security |</a>
+            <a href="https://news.ycombinator.com/security.html">Security</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://www.ycombinator.com/legal/">Legal |</a>
+            <a href="https://www.ycombinator.com/legal/">Legal</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="https://www.ycombinator.com/apply/">Apply to YC |</a>
+            <a href="https://www.ycombinator.com/apply/">Apply to YC</a>
           </div>
+          <div>|</div>
           <div>
-            <a href="#">Contact</a>
+            <a href="https://www.ycombinator.com/apply/">Contact</a>
           </div>
-          <form onSubmit={handleSubmit} method="Post">
-            <input
-              type="text"
-              search="input"
-              id="search"
-              onChange={(event) => setUserInput(event.target.value)}
-              value={userInput}
-              placeholder="Search..."
-            />
-            <button type="submit">Start Search</button>
-          </form>
         </div>
+        <form onSubmit={handleSubmit} method="Post">
+          <input
+            type="text"
+            search="input"
+            id="search"
+            onChange={(event) => setUserInput(event.target.value)}
+            value={userInput}
+            placeholder="Search..."
+          />
+          <button type="submit">Start Search</button>
+        </form>
       </div>
     </>
   );
